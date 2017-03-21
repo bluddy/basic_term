6052d0ca33e1922f81564a26b88d73d16e7ca5f7
Yotam Barnoy <yotambarnoy@gmail.com>
2017-03-21 15:24:24 -0400

    added comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "terminal.h"
 
+// This is an example application using the terminal class to create a simple
+// tic-tac-toe board. Note that this *isn't* an example of how to do any of this
+// *well*. It's a quickly slapped together example, and nothing here is idiomatic
+// object-oriented programming. The idea is to show you the things needed to make
+// something basic work.
+
+// draw a character of the board
 void drawChar(bool showCursor, int cursor[2], char arr[][4], int j, int i) {
     Terminal& t = Terminal::getTerm();
     char c = arr[j][i];
@@ -17,8 +24,10 @@ void drawChar(bool showCursor, int cursor[2], char arr[][4], int j, int i) {
     std::cout << std::flush;
 }
 
+// Draw the entire board
 void drawBoard(int cursor[2], char arr[3][4]) {
     Terminal& t = Terminal::getTerm();
+    // first clear the screen and then draw
     t.clearScreen();
     t.colorBg(Terminal::Black);
     for (int j=0; j<3; j++) {
@@ -30,14 +39,19 @@ void drawBoard(int cursor[2], char arr[3][4]) {
 }
 
 int main() {
+    // Primitive board
     char arr[3][4] = {"X O", "OXO", "X  "};
+    // Primitive cursor
     int cursor[2] = {1,1};
     Terminal& t = Terminal::getTerm();
+    // To draw with color and get unbuffered input, we need draw mode
     t.drawMode(true);
     drawBoard(cursor, arr);
 
     while (true) {
-        auto p = t.readChar();
+        // Read a character from the terminal
+        // Note that in Draw Mode, this is the *only* way to read!
+        std::pair<Terminal::Code, char> p = t.readChar();
         if ((p.first == Terminal::DownCode || p.second == 'j') &&
                 cursor[0] < 3) cursor[0]++;
         else if ((p.first == Terminal::UpCode || p.second == 'k') &&
@@ -49,7 +63,11 @@ int main() {
         else if (p.second == 'q') break;
         drawBoard(cursor, arr);
     }
+    // We're done with draw mode
     t.drawMode(false);
-    std::cout << "done";
+    // Now we can just do regular output/input
+    int x;
+    std::cin >> x;
+    std::cout << "done " << x;
 }
 
--- terminal.h
+++ terminal.h
@@ -1,20 +1,21 @@
-#ifndef TERMINAL
-#define TERMINAL
+#ifndef TERMINAL_H
+#define TERMINAL_H
 
 #include <iostream>
 
-// The Terminal class abstracts away things we want to do with the terminal
-// It is a singleton, and 
+// The Terminal class abstracts away things we need to draw to the terminal.
+// It's a singleton, so the only way to access it is to call the getTerm()
+// function: you can't construct this class.
 class Terminal {
 public:
-    enum Dir {
-        UpDir,
-        DownDir,
-        LeftDir,
-        RightDir
-    };
+    // Call this function to enter 'drawing mode'. In drawing mode, you paint the
+    // screen with characters of different background and foreground colors, and
+    // all input is immediately captured by the program, character by character.
+    // If you want to get back to normal reading/writing mode, turn this mode off.
     void drawMode(bool on);
 
+    // The possible colors. Note that foreground colors can also be 'bright',
+    // adding another 8 colors
     enum Color {
         Black = 0,
         Red,
@@ -26,13 +27,25 @@ public:
         White,
         Default = 9
     };
+    // Set the foreground color for the next piece of text, keeping the
+    // background color as is.
     void colorFg(bool bright, Color color);
+    // Set the background color for the next piece of text, keeping the
+    // foreground color as is.
     void colorBg(Color color);
+    // Set both background and foreground colors at the same time.
     void colorAll(bool bright, Color foreColor, Color backColor);
+    // Set negative mode for the next piece of text. This will reverse
+    // background and foreground colors, and is great for a cursor.
     void setNegative(bool set);
 
+    // Clear the screen and empty it. Do this before you draw your text image.
     void clearScreen();
 
+    // The possible characters we can return in Draw Mode. CharCode means that
+    // it's a regular character stored in the second member of the pair.
+    // Up/Down/Left/Right refer to the arrow keys.
+    // ErrorCode means something went wrong when reading input.
     enum Code {
         CharCode,
         UpCode,
@@ -41,15 +54,24 @@ public:
         RightCode,
         ErrorCode
     };
+    // Read a character of input in Draw Mode. This is the *only* way to get
+    // input in Draw Mode. No other method will work properly.
     std::pair<Code, char> readChar();
 
+    // Use this static function to get access to the Terminal singleton.
     static Terminal& getTerm();
 
     ~Terminal();
-    Terminal(const Terminal&) = delete;
 
 private:
+    enum Dir {
+        UpDir,
+        DownDir,
+        LeftDir,
+        RightDir
+    };
     Terminal();
+    Terminal(const Terminal&) = delete;
     static Terminal* g_terminal;
 
     void moveCursor(Dir dir, int n);
